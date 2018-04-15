af6b5e4e25a5c440ab787d756bbfeb31a758e907
Luis Robles <lfr4704@gmail.com>
2018-04-15 04:11:17 +0000

    revised

--- stocks_problem.js
+++ stocks_problem.js
@@ -19,12 +19,12 @@ let sortedYesterdayStockPrices = [];
 function getMaxProfit() {
 
     for (let earlierTime = 1; earlierTime < yesterdayStockPrices.length; earlierTime++) {
-        //this first if statement is to test if the scenario of a stock that lost money all day. 
+        //this first if statement is to test if the scenario of a stock that lost money all day. the profit will be negative
         if (yesterdayStockPrices[0] > yesterdayStockPrices[earlierTime] ) {
             
-            maxProfit = yesterdayStockPrices[yesterdayStockPrices.length-2] - yesterdayStockPrices[yesterdayStockPrices.length-1]
+            maxProfit = yesterdayStockPrices[yesterdayStockPrices.length-1] - yesterdayStockPrices[yesterdayStockPrices.length-2]
             
-        //the else statement is for the scenario where stock increased and decreased throught the day    
+        //the else statement is for the scenario where stock increased and decreased throught the day. profit will be positive  
         } else {
             console.log("entered the else section");
             sortedYesterdayStockPrices = yesterdayStockPrices.sort(compareNumber);
